cfe6044a70e95bb3f5dae1e4afc5f7a30cbe7c6e
user <user>
2020-01-15 14:19:35 +0000

    Added fetching data from the assets.

--- src/containers/List.js
+++ src/containers/List.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react';
 import Card from '../components/Card/Card';
 
 class List extends Component {
+    constructor() {
+	super();
+	this.state = { data: [], loading: true, };
+    };
+
+    async componentDidMount() {
+	const movies = await fetch('../../assets/data.json');
+	console.log(movies);
+	const moviesJSON = await movies.json();
+
+	if (moviesJSON) {
+	    this.setState({
+		data: moviesJSON,
+		loading: false
+	    });
+	}
+    };
+    
     render() {
-	return <Card />
+	const { data, loading } = this.state;
+
+	if (loading) {
+	    return <div>Loading...</div>
+	}
+
+	return data.map( movie => <Card key={ movie.id } movie={ movie } />);
     }
 };
 
