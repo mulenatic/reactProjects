1bfe3f85d9b42e42e1b00cea1ceafc60d0f8cf23
user <user>
2020-01-17 08:37:19 +0000

    Added the profile to the App, had to change css a bit.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import Header from '../components/Header/Header';
+import Profile from './Profile';
 
 function App() {
-  return (
-	  <div className="App">
+    return (
+	<div className="App">
 	  <Header logo={logo}/>
-    </div>
-  );
+	  <Profile />
+	</div>
+    );
 }
 
 export default App;
