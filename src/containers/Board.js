3b6ccdaeb8a2c704ce8a21f8d008f1fcfcc1f073
user <user>
2020-01-18 18:03:13 +0000

    Fetching data from json file and displaying it in Ticket components.

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -14,22 +14,52 @@ const BoardWrapper = styled.div`
 `;
 
 class Board extends Component {
-  render() {
-    const lanes = [
-      { id: 1, title: 'To Do' },
-      { id: 2, title: 'In Progress' },
-      { id: 3, title: 'Review' },
-      { id: 4, title: 'Done' },
-    ];
 
-    return (
-      <BoardWrapper>
-        {lanes.map(lane => (
-          <Lane key={lane.id} title={lane.title} />
-        ))}
-      </BoardWrapper>
-    );
-  }
+    constructor() {
+	super();
+	this.state = {
+	    data: [],
+	    loading: true,
+	    error: '',
+	}
+    }
+
+    async componentDidMount() {
+	try {
+	    const tickets = await fetch('../../assets/data.json');
+	    const ticketsJSON = await tickets.json();
+	    if (ticketsJSON) {
+		this.setState({
+		    data: ticketsJSON,
+		    loading: false,
+		});
+	    }
+	} catch(error) {
+	    this.setState({
+		loading: false,
+		error: error.message,
+	    });
+	}
+    }
+    
+    render() {
+	const {data, loading, error } = this.state;
+	
+	const lanes = [
+	    { id: 1, title: 'To Do' },
+	    { id: 2, title: 'In Progress' },
+	    { id: 3, title: 'Review' },
+	    { id: 4, title: 'Done' },
+	];
+
+	return (
+	    <BoardWrapper>
+              {lanes.map(lane => (
+		  <Lane key={lane.id} title={lane.title} loading={loading} error={error} tickets={data.filter(ticket => ticket.lane === lane.id)} />
+              ))}
+	    </BoardWrapper>
+	);
+    }
 }
 
 export default Board;
