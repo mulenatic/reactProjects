88cdcb73727d9bec44e219b0c4b3ae6b2aaed8aa
user <user>
2020-01-20 06:54:53 +0000

    Added react-router and set up a first route.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import Header from '../components/Header/Header';
 import Feed from './Feed';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -26,7 +27,9 @@ class App extends Component {
         <GlobalStyle />
         <AppWrapper>
           <Header />
-          <Feed />
+	  <Router>
+            <Route exact path='/' component={Feed} />
+	  </Router>
         </AppWrapper>
       </>
     );
